cb6c1b26e6bc60e58fa4dc17f2ec59d403a18407
benadidafouzia <benadidafouzia@gmail.com>
2024-03-18 15:30:57 +0300

    Squashed 'client/' content from commit 4e69a76
    git-subtree-dir: client
    git-subtree-split: 4e69a767f34d1e5dc3247322823f0a673f87ea76

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
+
+function App() {
+  // const { user } = useContext(AuthContext);
+  return (
+
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
