e3c79f8c04f934fef31468635921a29363781549
benadidafouzia <benadidafouzia@gmail.com>
2024-03-18 15:31:43 +0300

    Squashed 'api/' content from commit 36fdeee
    git-subtree-dir: api
    git-subtree-split: 36fdeee0e444f75acc43a9d419870d707d248b32

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+require('dotenv').config();
+const uri = process.env.MONGO_URL;
+
+mongoose.connect(uri)
+    .then(() => {
+        console.log('Connected to MongoDB');
+
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+app.listen(8080, () => {
+    console.log('server is running')
+})
+
+
