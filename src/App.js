87552c8c5e6a026831d88549e06b5f821a12cf00
benadidafouzia <benadidafouzia@gmail.com>
2024-03-18 11:11:19 +0300

    react router dom installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import Home from "./pages/home/Home"
+
+// import { useContext } from "react";
+// import { AuthContext } from "./context/AuthContext";
+
 function App() {
+  // const { user } = useContext(AuthContext);
   return (
-    <>
-      <Home />
-    </>
+
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/profile/:username">
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
 
   );
 }
